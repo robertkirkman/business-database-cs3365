ec2ec052f9ec3cf16248307b4a4eb03cb6dc1ace
Robert Kirkman <31490854+robertkirkman@users.noreply.github.com>
2020-02-14 16:50:58 -0600

    Update CustomerTable.java

--- CustomerTable.java
+++ CustomerTable.java
@@ -14,26 +14,100 @@ public class CustomerTable
 	private ArrayList<String> CustomerWorkPhone = new ArrayList<String>();
 	private ArrayList<String> CustomerCellPhone = new ArrayList<String>();
 	
+	//Concatenate customer information into a readable string for display
+	private String GetCustomerInfo(int FieldIndex)
+	{
+		return "Name: " + CustomerName.get(FieldIndex) + ". Address: " + CustomerAddress.get(FieldIndex)
+		+ ". Work Phone: " + CustomerWorkPhone.get(FieldIndex) + ". Cell Phone: " 
+		+ CustomerCellPhone.get(FieldIndex) + ".";
+	}
+	
 	//Query method
 	public String QueryTableField(String FieldName, String QueryString)
 	{
 		int FieldIndex;
-		if (FieldName == "Name")
-			if ((FieldIndex = CustomerName.indexOf(QueryString)) != -1)
-				return CustomerName.get(FieldIndex);
-			else return "Entry not found.";
-		else return "Fieldname not found.";
+		//use indexOf to search corresponding ArrayList for desired information
+		switch(FieldName)
+		{
+			case "Name":
+				{
+					if ((FieldIndex = CustomerName.indexOf(QueryString)) != -1)
+						return GetCustomerInfo(FieldIndex);
+					else return "Name entry not found.";
+				}
+			case "Address":
+				{
+					if ((FieldIndex = CustomerAddress.indexOf(QueryString)) != -1)
+						return GetCustomerInfo(FieldIndex);
+					else return "Address entry not found.";
+				}
+			case "Work Phone":
+				{
+					if ((FieldIndex = CustomerWorkPhone.indexOf(QueryString)) != -1)
+						return GetCustomerInfo(FieldIndex);
+					else return "Work phone entry not found.";
+				}
+			case "Cell Phone":
+				{
+					if ((FieldIndex = CustomerCellPhone.indexOf(QueryString)) != -1)
+						return GetCustomerInfo(FieldIndex);
+					else return "Cell phone entry not found.";
+				}
+			default:
+				return "Fieldname not found.";
+		}
 			
 	}
 	
 	//Modify method for existing entries
 	public String ModifyTableField(String FieldName, String QueryString, String EntryString)
 	{
-		
+		int FieldIndex;
+		switch(FieldName)
+		{
+			case "Name":
+				{
+					if ((FieldIndex = CustomerName.indexOf(QueryString)) != -1)
+					{
+						CustomerName.set(FieldIndex, EntryString);
+						return "Entry modified.";
+					}
+					else return "Entry not found.";
+				}
+			case "Address":
+				{
+					if ((FieldIndex = CustomerName.indexOf(QueryString)) != -1)
+					{
+						CustomerName.set(FieldIndex, EntryString);
+						return "Entry modified.";
+					}
+					else return "Entry not found.";
+				}
+			case "Work Phone":
+				{
+					if ((FieldIndex = CustomerName.indexOf(QueryString)) != -1)
+					{
+						CustomerName.set(FieldIndex, EntryString);
+						return "Entry modified.";
+					}
+					else return "Entry not found.";
+				}
+			case "Cell Phone":
+				{
+					if ((FieldIndex = CustomerName.indexOf(QueryString)) != -1)
+					{
+						CustomerName.set(FieldIndex, EntryString);
+						return "Entry modified.";
+					}
+					else return "Entry not found.";
+				}
+			default:
+				return "Fieldname not found.";
+		}
 	}
 	
-	//Modify method for new entries
-	private String ModifyTableField(String FieldName, String EntryString)
+	//Creation method for new entries
+	public String ExpandTableField(String FieldName, String EntryString)
 	{
 		
 	}
