279b3e5fabeb8e5a63415e2d0647d22e6fabdfde
Robert Kirkman <31490854+robertkirkman@users.noreply.github.com>
2020-02-14 15:42:52 -0600

    Work in progress client customer table class

---
+++ CustomerTable.java
@@ -0,0 +1,43 @@
+/* 
+ *  Author ......: Robert Kirkman
+ *  Course.......: CS 3365
+ *  Description..: Customer Table Class for Small Business Database
+ */
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class CustomerTable
+{
+	// Pre-chosen table fields using dynamically allocated arrays
+	private ArrayList<String> CustomerName = new ArrayList<String>();
+	private ArrayList<String> CustomerAddress = new ArrayList<String>();
+	private ArrayList<String> CustomerWorkPhone = new ArrayList<String>();
+	private ArrayList<String> CustomerCellPhone = new ArrayList<String>();
+	
+	//Query method
+	public String QueryTableField(String FieldName, String QueryString)
+	{
+		int FieldIndex;
+		if (FieldName == "Name")
+			if ((FieldIndex = CustomerName.indexOf(QueryString)) > 0)
+				return CustomerName.get(FieldIndex);
+			else return "Entry not found.";
+		else return "Fieldname not found.";
+			
+	}
+	
+	//Modify method for existing entries
+	public String ModifyTableField(String FieldName, String QueryString, String EntryString)
+	{
+		
+	}
+	
+	//Modify method for new entries
+	private String ModifyTableField(String FieldName, String EntryString)
+	{
+		
+	}
+	
+	//Deleting entries
+	private void DeleteFieldEntry(String FieldName, String QueryString)
+}
