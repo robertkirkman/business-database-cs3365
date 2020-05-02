3af42abb716b3afc642ba0e03d6bf9bd176a7dd1
lmilam <31598791+lmilam@users.noreply.github.com>
2020-05-01 22:30:55 -0500

    Update Table.java

--- Table.java
+++ Table.java
@@ -8,14 +8,17 @@ package smallbusinessdb;
 
 public interface Table
 {	
-	//methods to be overridden 
-	String QueryTableField(String FieldName, String QueryStr);
+	//methods to be overridden
+    	String GetInfo(int FieldIndex);
+	
+        String QueryTableField(String FieldName, String QueryStr);
 	
 	String ModifyTableField(String FieldName, String QueryStr, String EntryStr);
 	
 	String ExpandTableField(String FieldName, String EntryStr, String IndexStr);
 	
 	String DeleteFieldEntry(String FieldName, String QueryStr);
-	
-	int GetTableLength();
+
+        int GetTableLength();
+
 }
